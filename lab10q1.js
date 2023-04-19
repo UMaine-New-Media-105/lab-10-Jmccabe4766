c42fbb66580027a837a1fe0fd4d94ab729c61f47
Jmccabe4766 <124187641+Jmccabe4766@users.noreply.github.com>
2023-04-19 11:51:56 -0400

    Create lab10q1.js

---
+++ lab10q1.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  noStroke();
+  fill("green");
+  rect(0, 0, 400, 80);
+  rect(0, 200, 400, 60);
+  rect(0, 350, 400, 50);
+}
